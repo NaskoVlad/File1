64793cb7d4eb1b7a454f71d5d8cbb0c89d48d117
Vlad test <taranovivan666@gmail.com>
2024-06-27 11:51:06 +0300

    Второй коммит. 3 задачи решены

--- src/GameProgress.java
+++ src/GameProgress.java
@@ -1,7 +1,7 @@
 import java.io.Serializable;
 
 public class GameProgress implements Serializable {
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 11L;
 
     private int health;  // здоровье
     private int weapons;  // оружие
--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,89 @@
 import java.io.*;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
-        newDi();
+
 
         GameProgress gameProgress1 = new GameProgress(20, 50, 2, 105);
         GameProgress gameProgress2 = new GameProgress(50, 70, 10, 1105);
         GameProgress gameProgress3 = new GameProgress(80, 40, 7, 505);
 
+        newDi();
+        saveGame("savegames/game1.dat", gameProgress1);
+        saveGame("savegames/game2.dat", gameProgress2);
+        saveGame("savegames/game3.dat", gameProgress3);
+
+        zipFiles("savegames/zip1.zip", "savegames/game1.dat");
+        zipFiles("savegames/zip2.zip", "savegames/game2.dat");
+        zipFiles("savegames/zip3.zip", "savegames/game3.dat");
+
+        openZip("savegames/zip1.zip", "savegames");
+        openZip("savegames/zip2.zip", "savegames");
+        openZip("savegames/zip3.zip", "savegames");
+
+//        GameProgress gameProgress11 = openProgress("D:/Games/savegames/packed.txt");
+//        GameProgress gameProgress22 = openProgress("D:/Games/savegames/packed.txt");
+//        GameProgress gameProgress33 = openProgress("D:/Games/savegames/packed.txt");
+//        System.out.println(gameProgress11.toString());
+
     }
 
     public static void saveGame(String path, GameProgress gameProgress) {
-        try {
-            FileOutputStream fileOutputStream = new FileOutputStream(path);
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        try(FileOutputStream fileOutputStream = new FileOutputStream(path);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
             objectOutputStream.writeObject(gameProgress);
-            objectOutputStream.close();
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
 
-    public static void zipFiles(String pathArchive, String pathFile) {
+    public static GameProgress openProgress(String path){
+        GameProgress gameProgress = null;
+        try (FileInputStream fileInputStream = new FileInputStream(path);
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)){
+            gameProgress = (GameProgress) objectInputStream.readObject();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return  gameProgress;
+    }
 
+    public static void zipFiles(String pathArchive, String pathFile) {
         try (ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(pathArchive))) {
             FileInputStream fileInputStream = new FileInputStream(pathFile);
-            ZipEntry zipEntry = new ZipEntry("packed.txt");
+            File file = new File(pathFile);
+            ZipEntry zipEntry = new ZipEntry("text.txt");
             zipOutputStream.putNextEntry(zipEntry);
             byte[] buffer = new byte[fileInputStream.available()];
             fileInputStream.read(buffer);
             zipOutputStream.write(buffer);
-
-//            int byteRead = fileInputStream.read(buffer);
-//            while (byteRead != -1) {
-//                System.out.println((char) byteRead);
-//                byteRead = fileInputStream.read();
-//            }
-//            zipOutputStream.write(byteRead);
-
             zipOutputStream.closeEntry();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+    }
 
-
+    public static void openZip(String pathArchive, String pathFile){
+        try (ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(pathArchive))){
+            ZipEntry entry;
+            String name;
+            while ((entry = zipInputStream.getNextEntry()) != null){
+                name = entry.getName();
+                FileOutputStream fout = new FileOutputStream(pathFile + "/" + name );
+                for (int i = zipInputStream.read(); i != -1; i = zipInputStream.read()) {
+                    fout.write(i);
+                }
+                fout.flush();
+                zipInputStream.closeEntry();
+                fout.close();
+            }
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public static void newDi() throws IOException {
